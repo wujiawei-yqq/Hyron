a7431d93701ef23758aa9821b72e3ae8fa00e503
wjw-yqq <954797525@qq.com>
2020-12-02 22:55:00 +0800

    wjw

--- client/src/main/java/com/client/controller/ProviderController.java
+++ client/src/main/java/com/client/controller/ProviderController.java
@@ -2,11 +2,14 @@ package com.client.controller;
 
 import com.client.mapper.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/t")
 public class ProviderController {
 
     @Autowired
@@ -15,5 +18,11 @@ public class ProviderController {
     @RequestMapping(value = "/db")
     public String selectName(@RequestParam int Id) {
         return testMapper.selectName(Id);
+    }
+
+    @GetMapping("/index")
+    public String index(Model model){
+        model.addAttribute("ss","东方嘉盛分类考试了");
+        return "Hello"; //当浏览器输入/index时，会返回 /templates/home.html页面
     }
 }
